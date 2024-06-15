3bdb2659daef711c69085deb44d8bb66096dc3d9
Joseph Turian <>
2024-06-15 11:28:15 +0100

    Pre-load

--- predict.py
+++ predict.py
@@ -1,45 +1,95 @@
-# Prediction interface for Cog ⚙️
-# https://cog.run/python
-
-import csv
-import json
 import os
-import shutil
-import tempfile
-import os.path
+import pickle as pkl
+import sys
 
+import numpy as np
+import torch
+import yaml
 from cog import BasePredictor, Input, Path
+from tqdm import tqdm
+
+DOVER_PATH = "/DOVER"
+
+sys.path.append(DOVER_PATH)
+
+# Importing necessary modules from the DOVER package
+from dover.datasets import UnifiedFrameSampler, spatial_temporal_view_decomposition
+from dover.models import DOVER
 
 
 class Predictor(BasePredictor):
-    def setup(self) -> None:
-        """Load the model into memory to make running multiple predictions efficient"""
-        # self.model = torch.load("./weights.pth")
-        pass
+    def setup(self):
+        """Setup to load the model and configurations"""
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+
+        # Load configuration
+        config_path = os.path.join(DOVER_PATH, "dover.yml")
+        with open(config_path, "r") as f:
+            self.opt = yaml.safe_load(f)
+
+        # Initialize and load the model
+        self.model = DOVER(**self.opt["model"]["args"]).to(self.device)
+        self.model.load_state_dict(
+            torch.load(
+                os.path.join(DOVER_PATH, self.opt["test_load_path"]),
+                map_location=self.device,
+            )
+        )
+        self.model.eval()
+
+        self.mean = torch.FloatTensor([123.675, 116.28, 103.53]).to(self.device)
+        self.std = torch.FloatTensor([58.395, 57.12, 57.375]).to(self.device)
 
     def predict(
-        self,
-        video: Path = Input(description="Video to quality assess"),
+        self, video: Path = Input(description="Video to quality assess")
     ) -> str:
+        """Predict method to process video and output scores"""
+        video_path = str(video)
 
-        # Make a temporary directory
-        with tempfile.TemporaryDirectory() as temp_dir:
-            try:
-                # Copy the video to the temporary directory
-                video_path = os.path.join(temp_dir, os.path.basename(video))
-                print(f"Copying {video} to {video_path}")
-                shutil.copy(video, video_path)
-                tempcsv = os.path.join(temp_dir, "output.csv")
+        dopt = self.opt["data"]["val-l1080p"]["args"]
+        dopt["anno_file"] = None
+        dopt["data_prefix"] = os.path.dirname(video_path)
 
-                os.system(
-                    f"cd /DOVER ; python3 evaluate_a_set_of_videos.py --input_video_dir {temp_dir} --output_result_csv {tempcsv}"
+        temporal_samplers = {}
+        for stype, sopt in dopt["sample_types"].items():
+            if "t_frag" not in sopt:
+                temporal_samplers[stype] = UnifiedFrameSampler(
+                    sopt["clip_len"], sopt["num_clips"], sopt["frame_interval"]
                 )
-                lines = [r for r in csv.reader(open(tempcsv))]
-                assert len(lines) == 2
-                results = {k.strip(): float(v) for k, v in zip(lines[0], lines[1]) if k != "path"}
-                return json.dumps(results)
-            except:
-                raise
-            finally:
-                # Delete the temporary directory
-                shutil.rmtree(temp_dir)
+            else:
+                temporal_samplers[stype] = UnifiedFrameSampler(
+                    sopt["clip_len"] // sopt["t_frag"],
+                    sopt["t_frag"],
+                    sopt["frame_interval"],
+                    sopt["num_clips"],
+                )
+
+        # View Decomposition
+        views, _ = spatial_temporal_view_decomposition(
+            video_path, dopt["sample_types"], temporal_samplers
+        )
+
+        for k, v in views.items():
+            num_clips = dopt["sample_types"][k].get("num_clips", 1)
+            views[k] = (
+                ((v.permute(1, 2, 3, 0) - self.mean) / self.std)
+                .permute(3, 0, 1, 2)
+                .reshape(v.shape[0], num_clips, -1, *v.shape[2:])
+                .transpose(0, 1)
+                .to(self.device)
+            )
+
+        results = [np.mean(r.cpu().numpy()) for r in self.model(views)]
+        rescaled_results = self.fuse_results(results)
+
+        return json.dumps(rescaled_results)
+
+    def fuse_results(self, results):
+        """Fuse aesthetic and technical results into final scores"""
+        t, a = (results[1] - 0.1107) / 0.07355, (results[0] + 0.08285) / 0.03774
+        x = t * 0.6104 + a * 0.3896
+        return {
+            "aesthetic": 1 / (1 + np.exp(-a)),
+            "technical": 1 / (1 + np.exp(-t)),
+            "overall": 1 / (1 + np.exp(-x)),
+        }
