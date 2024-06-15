ea8c002fd263efe24c063ef3b629d3f40cbc04a3
Joseph Turian <>
2024-06-15 10:33:35 +0100

    Wrote a predict

--- predict.py
+++ predict.py
@@ -2,21 +2,36 @@
 # https://cog.run/python
 
 from cog import BasePredictor, Input, Path
-
-
+import os
+import tempfile
+import shutil
+import csv
+import json
 class Predictor(BasePredictor):
     def setup(self) -> None:
         """Load the model into memory to make running multiple predictions efficient"""
         # self.model = torch.load("./weights.pth")
+        pass
 
     def predict(
         self,
-        image: Path = Input(description="Grayscale input image"),
-        scale: float = Input(
-            description="Factor to scale image by", ge=0, le=10, default=1.5
-        ),
-    ) -> Path:
-        """Run a single prediction on the model"""
-        # processed_input = preprocess(image)
-        # output = self.model(processed_image, scale)
-        # return postprocess(output)
+        video: Path = Input(description="Video to quality assess"),
+    ) -> str:
+
+        # Make a temporary directory
+        with tempfile.TemporaryDirectory() as temp_dir:
+            try:
+                # Copy the video to the temporary directory
+                video_path = os.path.join(temp_dir, "video.mp4")
+                shutil.copy(video, video_path)
+                tempcsv = os.path.join(temp_dir, "output.csv")
+
+                os.system(f"cd /DOVER ; python3 evaluate_a_set_of_videos.py --input_video_dir {video_path} --output_result_csv {tempcsv}")
+                lines = [r for r in csv.open(tempcsv)]
+                assert len(lines) == 2
+                return json.dumps({k: v for k, v in zip(lines[0], lines[1])})
+            except:
+                raise
+            finally:
+                # Delete the temporary directory
+                shutil.rmtree(temp_dir)
